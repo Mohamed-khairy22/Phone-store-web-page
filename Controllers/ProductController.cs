f4859c79c64b117667b0a1d0e2c9952834c55490
mohamed-khairy <mohamedkhairy063036@gmail.com>
2024-07-09 19:09:06 +0300

    First Version of Phone store Web Page

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductSampleData sampleData = new ProductSampleData();
+
+        public IActionResult DEtails(int id)
+        {
+           Product productModel= sampleData.GetById(id);
+            return View("ProductDetails",productModel);
+            
+        }
+        public IActionResult VAll()
+        {
+            List<Product> productList = sampleData.GetAll();
+            return View("ShowAll", productList);
+        }
+
+        #region Test part
+        /*
+        public string show()
+        {
+            return "First view";
+        }
+        public ContentResult showContent()
+        {
+            ContentResult content = new ContentResult();
+            content.Content = "hello from contect";
+            return content;
+
+        }
+        public ViewResult showView()
+        {
+            ViewResult viewResult = new ViewResult();
+            viewResult.ViewName = "view1";
+            return viewResult;
+        }
+        public JsonResult showJson()
+        {
+            JsonResult result = new JsonResult(new { id = 1, name = "Mohamed" });
+            return result;
+        }
+        public IActionResult showMex(int id)
+        {
+            if (id == 1)
+            {
+                return View("view1");
+            }
+            else
+            {
+               
+                return Content("hello from contect");
+            }
+        }*/
+        #endregion
+    }
+}
---
+++ Models/ProductSampleData.cs
@@ -0,0 +1,23 @@
+﻿namespace Demo.Models
+{
+    public class ProductSampleData
+    {
+        List<Product> products;
+        public ProductSampleData()
+        {
+            products = new List<Product>();
+            products.Add(new Product() {Id=1 ,Name="IPhone",Price=50000,Images="1.jpeg",Description="IPhone13" });
+            products.Add(new Product() { Id = 2, Name = "IPhone", Price = 20000, Images = "2.jpeg", Description = "IPhoneX" });
+            products.Add(new Product() { Id = 3, Name = "IPhone", Price = 30000, Images = "3.jpeg", Description = "IPhone12" });
+
+        }
+        public List<Product> GetAll()
+        {
+            return products;
+        }
+        public Product GetById (int id)
+        { 
+        return products.FirstOrDefault(p=>p.Id==id);
+        }
+    }
+}
